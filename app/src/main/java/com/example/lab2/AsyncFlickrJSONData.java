169bd0e38de83b8fe0321b2ffcae6cc00f7b2b61
Teeflam <difranck@live.fr>
2021-02-21 22:56:34 +0100

    exo 15-16

--- app/src/main/java/com/example/lab2/AsyncFlickrJSONData.java
+++ app/src/main/java/com/example/lab2/AsyncFlickrJSONData.java
@@ -1,13 +1,71 @@
 package com.example.lab2;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class AsyncFlickrJSONData implements AsyncTask<String, Void, JSONObject> {
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class AsyncFlickrJSONData extends AsyncTask<String, Void, JSONObject> {
     @Override
     protected JSONObject doInBackground(String... params) {
-        return null;
+        JSONObject result = null;
+
+        try {
+            // retrieve the url
+            URL url = new URL(params[0]);
+
+            // connect to the url
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+
+            try {
+                // get data
+                InputStream is = new BufferedInputStream(urlConnection.getInputStream());
+                String s = readStream(is);
+
+                // transform it into a JSONObject
+                result = new JSONObject(s);
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
+    @Override
+    protected void onPostExecute(JSONObject result) {
+        try {
+            Log.i("JFL", result.toString(4));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readStream(InputStream is) {
+        try {
+            // initialize the output stream
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+
+            // retrieve the output content
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
 }
--- app/src/main/java/com/example/lab2/GetImageOnClickListener.java
+++ app/src/main/java/com/example/lab2/GetImageOnClickListener.java
@@ -7,6 +7,6 @@ public class GetImageOnClickListener implements View.OnClickListener {
     @Override
     public void onClick(View v){
         //start a new asynctask of type AsyncFlickrJSONData
-        new AsyncFlickrJSONData().execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=cats&format=json");
+        new AsyncFlickrJSONData().execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=cats&format=json&nojsoncallback=1");
     }
 }
