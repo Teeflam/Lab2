200a4d0e7ea2315cd709f2b4d438740c3c34d8b4
Teeflam <franck.di@efrei.net>
2021-02-24 12:16:18 +0100

    exo19

--- app/src/main/java/com/example/lab2/Flickrapp.java
+++ app/src/main/java/com/example/lab2/Flickrapp.java
@@ -2,9 +2,11 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.view.View;
 
 public class Flickrapp extends AppCompatActivity {
 
@@ -16,8 +18,15 @@ public class Flickrapp extends AppCompatActivity {
         //get the image container
         ImageView image = findViewById(R.id.image);
 
-        Button getImage = (Button)findViewById(R.id.getImage);
+        Button getImage = (Button) findViewById(R.id.getImage);
         getImage.setOnClickListener(new GetImageOnClickListener(image));
     }
+    // handle when the user want to get to the list activity
+    public void onClickList(View v) {
+        // start new activity
+        Intent list = new Intent(getApplicationContext(), ListActivity.class);
+        startActivity(list);
+    }
 }
+
 
